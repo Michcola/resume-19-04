4cb1aa693dcd3e7c41e74eb0bc449df8117ffe52
mich <843298603@qq.com>
2019-04-18 20:27:50 +0800

    用MVC实现

--- js/message.js
+++ js/message.js
@@ -1,41 +1,13 @@
 !function () {
+    var model = Model({ resourceName:'Message' })
 
-    var model = {
-        //获取数据
-        init: function () {
-            var APP_ID = 'WJYoikRJ4DxQUjfWCVt7LsdR-gzGzoHsz'
-            var APP_KEY = '4V9bbGQPenDckK6ypd8KA3Hu'
-            AV.init({ appId: APP_ID, appKey: APP_KEY })
-        },
-        fetch: function () {
-            var query = new AV.Query('Message');
-            return query.find() //Promise对象
-        },
-        //创建数据
-        save: function (name, content) {
-            var Message = AV.Object.extend('Message');
-            var message = new Message();
-            return message.save({  //Promise 对象
-                'name': name,
-                'content': content
-            })
-        }
-    }
-    var view = document.querySelector('section.message')
+    var view = View('section.message')
 
-
-    var controller = {
-        view: null,
-        model: null,
-        messageList: null,
-        init: function (view,model) {
-            this.view = view
-            this.model= model
+    var controller = Controller({
+        init: function(view, model){
             this.messageList = view.querySelector('#messageList')
             this.form = view.querySelector('form')
-            this.model.init()
             this.loadMessages()
-            this.bindEvents()
         },
         loadMessages: function () {
             this.model.fetch().then(
@@ -59,7 +31,9 @@
             let myForm = this.form
             let name = myForm.querySelector('input[name=name]').value
             let content = myForm.querySelector('input[name=content]').value
-            this.model.save(name,content).then(function (object) {
+            this.model.save({
+                'name':name,'content':content
+                }).then(function (object) {
                 let li = document.createElement('li')
                 li.innerText = `${object.attributes.name}:${object.attributes.content} `
                 let messageList = document.querySelector('#messageList')
@@ -68,8 +42,9 @@
                 console.log(object)
             })
         }
+    })
 
-    }
+ 
 
     controller.init(view,model)
 }.call()
