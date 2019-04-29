4e91ce64cd75af2191210452b7c5538d8d69832f
mich <843298603@qq.com>
2019-04-30 00:12:45 +0800

    add dist

---
+++ src/js/base/Model.js
@@ -0,0 +1,25 @@
+/*
+var model = Model({
+    resourceName:'表名'
+})
+*/
+
+window.Model = function (options) {
+    let resourceName = options.resourceName
+    return {
+        init: function () {
+            var APP_ID = 'WJYoikRJ4DxQUjfWCVt7LsdR-gzGzoHsz'
+            var APP_KEY = '4V9bbGQPenDckK6ypd8KA3Hu'
+            AV.init({ appId: APP_ID, appKey: APP_KEY })
+        },
+        fetch: function () {
+            var query = new AV.Query(resourceName);
+            return query.find() //Promise对象
+        },
+        save: function (object) {
+            var x = AV.Object.extend(resourceName);
+            var x = new x();
+            return x.save(object)
+        }
+    }
+}
---
+++ src/js/base/View.js
@@ -0,0 +1,8 @@
+/*
+var view = View('.xxx')
+*/
+
+window.View = function (selector) {
+    return document.querySelector(selector)
+}
+
